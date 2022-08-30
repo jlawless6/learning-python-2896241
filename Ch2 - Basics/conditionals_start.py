6bb1f4247c8cc9ef3ac52980365454ae70dd3096
jlawless <joel.lawless@cox.com>
2022-08-30 11:02:25 -0400

    Working on conditionals / uses newer python ver

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -5,15 +5,39 @@
 
 
 
+from unittest import result
+
+
 def main():
-    x, y = 10, 100
+    x, y = 10, 1000
 
-    # conditional flow uses if, elif, else
+    # # conditional flow uses if, elif, else
+    # if x < y:
+    #     result = "x is less than y"
+    # elif x == y:
+    #     result = "x is the same as y"
+    # else:
+    #     result = "x is greater than y"
 
-    # conditional statements let you use "a if C else b"
+    # print(result)
+
+    # # conditional statements let you use "a if C else b"
+    # result = "x is less than y" if x < y else "x is greater or equal to y"
+    # print(result)
 
     # match-case makes it easy to compare multiple values
     value = "one"
+    match value:
+        case "one":
+            result = 1
+        case "two":
+            result = 2
+        case "three" | "four":
+            result = (3,4)
+        case _:
+            result = -1
+    print(result)
+    
 
 if __name__ == "__main__":
     main()
